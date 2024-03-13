527d80ca532c625de0bea2c89f7d6c3fd0733567
Mohammed Taqi <taqealibade@gmail.com>
2024-03-13 15:03:24 +0300

    Dynamic Route Params in Nuxt.js

---
+++ pages/teams/[teamSlag]/users/[userId].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>
+            {{ message }}
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const message = computed(() => `I'm on team ${ route.params.theamSlug }
+            and my user id is ${ route.params.userId }`)
+</script>
