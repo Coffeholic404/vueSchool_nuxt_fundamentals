663432b09f0ffbdb66bd9b7c6e0a698fb60a5c43
Mohammed Taqi <taqealibade@gmail.com>
2024-03-14 06:18:26 +0300

    Create Components that Auto Import

---
+++ components/movie/MovieSearch.vue
@@ -0,0 +1,26 @@
+<script  setup>
+  const query = ref("")
+  const movies = ref([])
+  const  search = async () => {
+    const { Search } = await $fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=9357a82d&s=${query.value}`)
+    movies.value =Search
+  }
+</script>
+
+<template>
+  <div>
+  <form @submit.prevent="search">
+    <input type="text" name="" id="" v-model="query"> 
+    <button>Search</button>
+  </form>
+  <ul style="display: flex; flex-wrap: wrap; gap: 1rem; list-style: none;">
+    <li v-for="movie in movies" :key="movie.imdbID">
+    <NuxtLink :to="{name: 'movies-id', params: {id: movie.imdbID}}">
+    <img :src="movie.Poster" :alt="movie.title">
+    </NuxtLink>
+    </li>
+  </ul>
+</div>
+</template>
+
+<style scoped></style>
